e997418c7dfac24f670811bb8034a70d158fa7da
Roberto Guzman <rjguzman42@Robertos-MacBook-Pro.local>
2019-04-18 14:29:37 -0400

    initial upload

---
+++ Reachability/Reachability/Reachability.h
@@ -0,0 +1,19 @@
+//
+//  Reachability.h
+//  Reachability
+//
+//  Created by Roberto Guzman on 4/18/19.
+//  Copyright © 2019 Fortytwo Sports. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Reachability.
+FOUNDATION_EXPORT double ReachabilityVersionNumber;
+
+//! Project version string for Reachability.
+FOUNDATION_EXPORT const unsigned char ReachabilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Reachability/PublicHeader.h>
+
+
